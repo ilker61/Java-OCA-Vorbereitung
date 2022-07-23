c3f0b804b3869366071c0741094db9974daf4395
ilker61 <jakyuer@gmail.com>
2022-07-23 21:46:55 +0200

    23.07.22

--- src/day05_increment/C01_PreIncrement_PostIncrement.java
+++ src/day05_increment/C01_PreIncrement_PostIncrement.java
@@ -3,6 +3,20 @@ package day05_increment;
 public class C01_PreIncrement_PostIncrement {
     public static void main(String[] args) {
 
+    int sayi=10;
+    sayi++;
+        System.out.println(sayi);
+
+    sayi++;
+    System.out.println(sayi);// 12
+
+        System.out.println(++sayi);// 13
+
+        System.out.println(sayi++);// 13
+
+        System.out.println(sayi);// 14
+
+
 
     }
 
