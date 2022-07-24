8b0c710f600de7e6df5d457341f301087edd4522
ilker61 <jakyuer@gmail.com>
2022-07-24 14:47:50 +0200

    24.07.2022 Concatenation

--- src/day06_concatenation/C01_Concatenation.java
+++ src/day06_concatenation/C01_Concatenation.java
@@ -1,7 +1,55 @@
 package day06_concatenation;
 
+import java.util.Scanner;
+
 public class C01_Concatenation {
     public static void main(String[] args) {
+
+        // Kullanicidan alinan 4 basamakli sayinin rakamlar toplamini bul
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("4 basamkli bir sayi gir");
+
+        int sayi=scan.nextInt();
+
+        int rakamlarToplami=0;
+
+        // sayi 6789 olsun
+
+        /*
+
+        1. yol
+
+        rakamlarToplami+=sayi%10;  // 9
+        sayi/=10; // 678
+
+        rakamlarToplami+=sayi%10; // 17
+        sayi/=10; // 67
+
+        rakamlarToplami+=sayi%10; // 24
+        sayi/=10; // 6
+
+        rakamlarToplami+=sayi%10; // 30
+        sayi/=10; // 0
+
+        System.out.println(rakamlarToplami);
+
+         */
+
+
+        // 2. yol
+
+        for (int i = 0; i <= sayi; i++) {
+            rakamlarToplami+=sayi%10;
+            sayi/=10;
+        }
+        System.out.println(rakamlarToplami);
+
+
+
+
+
 
 
 
