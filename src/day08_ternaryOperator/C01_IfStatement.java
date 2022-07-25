7351e7f90d1c7c5191e8662a912b9b3d7bd6ab1c
ilker61 <jakyuer@gmail.com>
2022-07-25 12:14:32 +0200

    25.07.2022 Ternary Operator

--- src/day08_ternaryOperator/C01_IfStatement.java
+++ src/day08_ternaryOperator/C01_IfStatement.java
@@ -1,9 +1,22 @@
 package day08_ternaryOperator;
 
+import java.util.Scanner;
+
 public class C01_IfStatement {
     public static void main(String[] args) {
 
+        // Girilen bir karakterin harf olup olmadigini bulalim
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir karakter girin");
+
+        char karakter=scan.next().charAt(0);
+
+        if((karakter>='A' && karakter<='Z')  || (karakter>='a' && karakter<='z') ){
+            System.out.println("Girdiginiz karakter bir harf");
+        } else {
+            System.out.println("Girdiginiz her ne nane ise harf degildur");
+        }
 
     }
 }
