c7a56f30bdd5a1999a8fbd719287e5693e2a5c2c
ilker61 <jakyuer@gmail.com>
2022-07-25 20:10:57 +0200

    25.07.2022 switch case

---
+++ src/day10_switch.java
@@ -0,0 +1,32 @@
+public class day10_switch {
+    public static void main(String[] args) {
+
+        int sayi=3;
+
+        switch (sayi){
+            case 1:
+                System.out.println("sayi=1");
+                break;
+            case 2:
+                System.out.println("sayi=2");
+                break;
+            case 3:
+                System.out.println("sayi=3");
+                break;
+            case 4:
+                System.out.println("sayi=4");
+                break;
+            default:
+                System.out.println("sayi bunlardan biri degil");
+        }
+
+        /*
+
+            ###########################################################
+            Switch Case de long, double, float ve boolean KULLANILMAZ
+            ###########################################################
+
+         */
+
+    }
+}
