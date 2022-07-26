e2832e489afb51510572133dc9101faba690d3fa
ilker61 <jakyuer@gmail.com>
2022-07-26 20:54:23 +0200

    26.07.2022 string manipulation

--- src/day11_stringManipulation/C05_IndexOf.java
+++ src/day11_stringManipulation/C05_IndexOf.java
@@ -1,5 +1,7 @@
 package day11_stringManipulation;
 
+import java.util.Scanner;
+
 public class C05_IndexOf {
     public static void main(String[] args) {
 
@@ -11,6 +13,28 @@ public class C05_IndexOf {
         System.out.println(str1.length()-1); // 19
 
         System.out.println(str1.indexOf("bir")); // 5
+
+
+        // Soru; Kullanicidan bir cumle ve bir kelime iste.
+        // kelime cumlenin icinde varmi kontrol et
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("Lutfen bir cumle giriniz");
+
+        String cumle=scan.nextLine();
+
+        System.out.println("Lutfen bir kelime giriniz");
+
+        String kelime=scan.next();
+
+        if (cumle.contains(kelime)){
+            System.out.println("cumle kelimeyi iceriyor");
+        }
+        else {
+            System.out.println("cumle kelimeyi icermiyi");
+        }
+
 
 
     }
