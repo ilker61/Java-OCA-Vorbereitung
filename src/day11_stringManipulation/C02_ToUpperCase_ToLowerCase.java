ffee976bbba2548a6e304589c73477a4893af60c
ilker61 <jakyuer@gmail.com>
2022-07-26 12:36:49 +0200

    26.07.2022 string manipulation

--- src/day11_stringManipulation/C02_ToUpperCase_ToLowerCase.java
+++ src/day11_stringManipulation/C02_ToUpperCase_ToLowerCase.java
@@ -1,5 +1,7 @@
 package day11_stringManipulation;
 
+import java.util.Locale;
+
 public class C02_ToUpperCase_ToLowerCase {
     public static void main(String[] args) {
 
@@ -11,11 +13,17 @@ public class C02_ToUpperCase_ToLowerCase {
         System.out.println(str.toLowerCase().charAt(5)); // g
 
         System.out.println(str.toUpperCase());  // JAVA GUZELDIR
+        System.out.println(str.toUpperCase(Locale.forLanguageTag("tr")));  // JAVA GUZELDİR (Turkce karakte ile)
 
         System.out.println(str.toLowerCase());  // java guzeldir
 
+        String name= "Ali Can";
+        String isim="Ali Can";
+        String ad= "ali can";
 
+        System.out.println(name.toLowerCase().equals(ad)); // true
 
+        System.out.println(name.equals(isim)); // true
 
     }
 }
