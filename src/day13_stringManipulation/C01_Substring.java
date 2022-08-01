bf9642a30a2cf45a9023684c80df5924f5dc59cf
ilker61 <jakyuer@gmail.com>
2022-08-01 21:33:57 +0200

    01.08.2022 string manipulation

--- src/day13_stringManipulation/C01_Substring.java
+++ src/day13_stringManipulation/C01_Substring.java
@@ -8,5 +8,10 @@ public class C01_Substring {
         System.out.println(str.substring(0, 1).replace("J", "x"));
         System.out.println(str);
 
+       // System.out.println(str.substring(5,2)); // Exception in thread "main" java.lang.StringIndexOutOfBoundsException
+
+        System.out.println(str.substring(str.length()-1)); // l
+        System.out.println(str.substring(str.length())); // hiclik verir ""
+
     }
 }
