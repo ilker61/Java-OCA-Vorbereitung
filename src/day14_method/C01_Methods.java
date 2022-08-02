50731e9f6f5171c4f1c61a20eb8a4f8020bd038e
ilker61 <jakyuer@gmail.com>
2022-08-02 10:29:12 +0200

    02.08.2022 soru

--- src/day14_method/C01_Methods.java
+++ src/day14_method/C01_Methods.java
@@ -1,8 +1,22 @@
 package day14_method;
 
+import java.util.Scanner;
+
 public class C01_Methods {
     public static void main(String[] args) {
 
-        // de yuru loo
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Lutfi 4 harfli bir kelime gir");
+
+        String kelime=scanner.next();  // adem
+        String tersKelime;
+
+        for (int i = 0; i <=kelime.length()-1 ; i++) {
+
+            tersKelime=kelime.substring(kelime.length()-i);
+            System.out.println(tersKelime);
+        }
+
     }
 }
