bee289548230ca1d97575ec2e040d431424f664d
ilker61 <jakyuer@gmail.com>
2022-08-03 12:12:27 +0200

    03.08.2022 soru

---
+++ src/day18_nestedForloop/C03_WhileLoop.java
@@ -0,0 +1,22 @@
+package day18_nestedForloop;
+
+public class C03_WhileLoop {
+    public static void main(String[] args) {
+
+        // Kullanicidan biki tam sayi al ve bu sayilari ve aralarindaki sayilari
+        // yazdiran bir kod olusturah
+
+        int baslangic=40;
+        int bitis=60;
+
+        while (baslangic<=bitis){
+            System.out.print(baslangic + " ");
+            baslangic++;
+        }
+        System.out.println("");
+
+        System.out.println(baslangic);
+
+
+    }
+}
