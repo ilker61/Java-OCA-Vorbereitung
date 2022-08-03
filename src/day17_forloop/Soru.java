d0f916fded843bd130c905aef97f4d602e70296b
ilker61 <jakyuer@gmail.com>
2022-08-03 09:38:40 +0200

    03.08.2022 soru

---
+++ src/day17_forloop/Soru.java
@@ -0,0 +1,13 @@
+package day17_forloop;
+
+public class Soru {
+    public static void main(String[] args) {
+
+        function(1 , 3);
+
+    }
+
+    public static void function(float i, int j){
+        System.out.println("Bad");
+    }
+}
