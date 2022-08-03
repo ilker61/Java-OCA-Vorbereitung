4a6c09b5e55459372598f733a4063938bbe5dabd
ilker61 <jakyuer@gmail.com>
2022-08-03 12:02:03 +0200

    03.08.2022 soru

---
+++ src/day18_nestedForloop/C01_NestedForloopCarpimTabl.java
@@ -0,0 +1,26 @@
+package day18_nestedForloop;
+
+public class C01_NestedForloopCarpimTabl {
+
+    // Kullanicidan bir rakkam alip carpim tablosu olusturaah
+
+    public static void main(String[] args) {
+
+        int input= 4;
+
+        // 1  2  3        1*1    1*2   1*3
+        // 2  4  6        2*1    2*2   2*3
+        // 3  6  9        3*1    3*2   3*3
+
+
+        for (int i = 1; i <=input ; i++) {
+            for (int j = 1; j <=input ; j++) {
+                System.out.print((i*j)+"     ");
+            }
+            System.out.println("");
+        }
+
+
+
+    }
+}
---
+++ src/day18_nestedForloop/C02_NestFrLoopAfilliSekiller.java
@@ -0,0 +1,29 @@
+package day18_nestedForloop;
+
+public class C02_NestFrLoopAfilliSekiller {
+
+    public static void main(String[] args) {
+
+        int input= 5;
+
+        // 1  2  3        1*1    1*2   1*3
+        // 2  4  6        2*1    2*2   2*3
+        // 3  6  9        3*1    3*2   3*3
+
+
+        for (int i = 1; i <=input ; i++) {
+            for (int j = 1; j <=input ; j++) {
+                System.out.print((i*j)+"     ");
+            }
+            System.out.println("");
+        }
+
+        for (int i = 1; i <=input ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
