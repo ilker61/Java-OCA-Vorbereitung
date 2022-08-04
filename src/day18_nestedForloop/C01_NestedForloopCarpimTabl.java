f8d7191cf2cb7dea65d7e3e606fbf55d88257d33
ilker61 <jakyuer@gmail.com>
2022-08-04 23:14:20 +0200

    04.08.2022 Array

--- src/day18_nestedForloop/C01_NestedForloopCarpimTabl.java
+++ src/day18_nestedForloop/C01_NestedForloopCarpimTabl.java
@@ -12,14 +12,12 @@ public class C01_NestedForloopCarpimTabl {
         // 2  4  6        2*1    2*2   2*3
         // 3  6  9        3*1    3*2   3*3
 
-
-        for (int i = 1; i <=input ; i++) {
-            for (int j = 1; j <=input ; j++) {
-                System.out.print((i*j)+"     ");
+        for (int i = 1; i <=3 ; i++) {
+            for (int j = 1; j <=3 ; j++) {
+                System.out.print(i*j + "  ");
             }
             System.out.println("");
         }
-
 
 
     }
