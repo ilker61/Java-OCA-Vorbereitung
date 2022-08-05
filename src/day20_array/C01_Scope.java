f694e2fc3740f2183ed3ff95544f3c46519700d5
ilker61 <jakyuer@gmail.com>
2022-08-05 10:05:59 +0200

    05.08.2022 Array

---
+++ src/day20_array/C01_Scope.java
@@ -0,0 +1,16 @@
+package day20_array;
+
+public class C01_Scope {
+    int sayi;
+    String bransIsmi="Java";
+    boolean okulAcikMi;
+
+    public static void main(String[] args) {
+
+        C01_Scope obj1=new C01_Scope();
+        System.out.println(obj1.okulAcikMi);
+
+    }
+
+
+}
--- src/day20_array/C02_Array.java
+++ src/day20_array/C02_Array.java
@@ -1,6 +1,6 @@
 package day20_array;
 
-public class C01_Array {
+public class C02_Array {
     public static void main(String[] args) {
 
 
