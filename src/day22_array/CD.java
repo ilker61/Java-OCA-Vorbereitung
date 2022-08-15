f990611f245b9413ec63c9b9bdc9cd5cce1ffc96
ilker61 <jakyuer@gmail.com>
2022-08-15 13:37:19 +0200

    15.08.2022 oca

---
+++ src/day22_array/CD.java
@@ -0,0 +1,8 @@
+package day22_array;
+
+public class CD {
+    int r;
+    CD (int r){
+        this.r=r;
+    }
+}
---
+++ src/day22_array/DVD.java
@@ -0,0 +1,9 @@
+package day22_array;
+
+public class DVD extends CD{
+    int c;
+    DVD (int r, int c){
+        super(r);
+        this.c=c;
+    }
+}
